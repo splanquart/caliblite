cfae7eb92761613e6321680fed3e8c4c55f146ed
Stéphane Planquart <stephane@planquart.com>
2022-07-03 20:11:18 +0200

    code format

--- src/switcher.py
+++ src/switcher.py
@@ -1,17 +1,21 @@
 import serial
 
 
-on = b'\xA0\x01\x01\xA2'
-off = b'\xA0\x01\x00\xA1'
+on = b"\xA0\x01\x01\xA2"
+off = b"\xA0\x01\x00\xA1"
+
 
 class _Simulator:
     def __init(self):
         pass
+
     def close(self):
         pass
+
     def write(self, data):
         pass
-        
+
+
 class Switcher:
     def __init__(self, port, debug=False):
         if port == "Simulator":
@@ -25,20 +29,20 @@ class Switcher:
     def _talk(self):
         if self.debug:
             print(f"Switcher is {self.is_on}")
-    
+
     def close(self):
         self.ser.close()
 
     def switch_on(self):
-        self.is_on=True
+        self.is_on = True
         self.ser.write(on)
         self._talk()
 
     def switch_off(self):
-        self.is_on=False
+        self.is_on = False
         self.ser.write(off)
         self._talk()
-    
+
     def toggle(self):
         if self.is_on:
             self.switch_off()
